d43decf45549db34a3cb802015f7b57797fd52ad
sagar <sagar@SAGAR>
2026-01-31 16:26:25 +0530

    Add project files.

---
+++ SchoolAdministrator/Program.cs
@@ -0,0 +1,15 @@
+﻿using oops;
+
+namespace SchoolAdministrator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            SchoolAdministrstor admin = new SchoolAdministrstor();
+            admin.ManageSchool();
+        }
+    }
+}
