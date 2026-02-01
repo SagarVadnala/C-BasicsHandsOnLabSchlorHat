681e23f95f2a23cdc796b154ecf7b4418a341ea8
Vadnala Sagar <sagarvadnal77@gmail.com>
2026-02-01 13:40:28 +0530

    added Loops and statements with few partens

--- HandsOnLabSchlorHat/Program.cs
+++ HandsOnLabSchlorHat/Program.cs
@@ -6,11 +6,19 @@
         {
             Console.WriteLine("GettingStartedBasics :");
 
-            GettingStartedBasics basics = new GettingStartedBasics();   
-           // basics.AreaOfSqr();
+            GettingStartedBasics basics = new GettingStartedBasics();
+            // basics.AreaOfSqr();
             //basics.SwapWithSeconVar();
             //basics.SwapWithOutSeconVar();
-            basics.SumOfAllNum();
+            //basics.SumOfAllNum();
+
+            StatementsAndLoops statementsAndLoops = new StatementsAndLoops();
+            //statementsAndLoops.CheckCharIsAlphaOrNot();
+            //statementsAndLoops.CheckNumIsEvenOrOdd();
+           // statementsAndLoops.RighttrianglePattern();
+            statementsAndLoops.LeftTrianglePattern();
+
+
         }
     }
 }
